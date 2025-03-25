e7b60e365be8e4dcb756f34bdbc153c43dd7dc83
Vlad Mishustin <riololfundud@mail.ru>
2025-03-25 17:03:44 +0100

    è il mio primo commit nella vita

---
+++ src/main/java/com/bss/web_prova/WebProvaApplication.java
@@ -0,0 +1,13 @@
+package com.bss.web_prova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebProvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebProvaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bss/web_prova/WebProvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bss.web_prova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
